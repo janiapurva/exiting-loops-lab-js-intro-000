708581b5b23cc51b68baf1ba528e82849b932353
APURVA MAHNEDRABHAI JANI <jani.apurva@yahoo.com>
2020-09-18 17:28:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function loops breakOut(array,changeValue,stopvalue){
+function loops breakOut(array,stopvalue){
   for(var i=0;i<array.lenth;i++){
     if(array[i]==stopValue){
+      break;
       return array[i]
     }
   }
